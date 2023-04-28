00e1b36f15a932a93e0840945c933a4fc31c328f
Abhishek Kumar <dk9554654249@gmail.com>
2023-04-29 01:01:55 +0530

    fixed  accordion

--- src/components/HomePage/FAQs/FAQs.js
+++ src/components/HomePage/FAQs/FAQs.js
@@ -1,20 +1,24 @@
 
 import classes from "./faqs.module.css";
 import Question from "./Question";
-const FAQs=()=>{
-    return(
+const FAQs = () => {
+    return (
         <div className={classes.faqSection}>
             <p>
                 Frequently Asked Questions
             </p>
             <h1>Helping brands succeed</h1>
-            <div className={classes.questions}>
-                <Question/>
-                <Question/>
-                <Question/>
-                <Question/>
-                <Question/>
-                <Question/>
+            <div className={classes.accordion}>
+                <div className={classes.col1}>
+                    <Question />
+                    <Question />
+                    <Question />
+                </div>
+                <div className={classes.col2}>
+                    <Question />
+                    <Question />
+                    <Question />
+                </div>
             </div>
         </div>
     )
